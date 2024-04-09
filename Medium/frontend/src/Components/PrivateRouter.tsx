42caf9da46a63c0152781a33d3de79639f5c0be4
Yogesh <chauhanyogesh950@gmail.com>
2024-04-09 20:17:50 +0530

    refresh bug removed

--- Medium/frontend/src/Components/PrivateRouter.tsx
+++ Medium/frontend/src/Components/PrivateRouter.tsx
@@ -1,15 +1,16 @@
-import { ReactNode, useContext } from 'react'
+import { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
-import { AuthContext } from '../Context/AuthContext'
+
 
 interface PrivateRouterProps {
   children: ReactNode;
 }
 
 const PrivateRouter: React.FC<PrivateRouterProps> = ({ children }) => {
-  const { isAuth } = useContext(AuthContext);
 
-  if (!isAuth) {
+  const auth = localStorage.getItem("token")? true:false
+
+  if (!auth) {
     return <Navigate to={"/"} />;
   }
 
